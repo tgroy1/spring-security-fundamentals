93a440c6c9937f1d1c0c1922acd1a377d7f35b74
Tiyas <tgroy1@gmail.com>
2021-07-28 12:45:48 +0530

    Spring Security basics

---
+++ README.md
@@ -0,0 +1,6 @@
+# spring-security-fundamentals
+
+**spring-security**
+  1. Basic Authentication with AuthenticationManagerBuilder configuration
+  2. Basic Authorization with HttpSecurity configuration
+
---
+++ spring-security/src/main/java/com/tgroy/springsecurity/config/SecurityConfiguration.java
@@ -0,0 +1,42 @@
+package com.tgroy.springsecurity.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.password.NoOpPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+
+@SuppressWarnings("deprecation")
+@EnableWebSecurity
+public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
+	
+	@Override
+	public void configure(AuthenticationManagerBuilder auth) throws Exception {
+		
+		auth.inMemoryAuthentication()
+			.withUser("user")
+			.password("user")
+			.roles("USER")
+			.and()
+			.withUser("admin")
+			.password("admin")
+			.roles("ADMIN");
+	}
+	
+	@Bean
+	public PasswordEncoder getPasswordEncoder() {
+		return NoOpPasswordEncoder.getInstance();
+	}
+
+	@Override
+	protected void configure(HttpSecurity http) throws Exception {
+		http.authorizeRequests()
+			.antMatchers("/admin").hasRole("ADMIN")
+			.antMatchers("/user").hasAnyRole("USER", "ADMIN")
+			.antMatchers("/*").permitAll()
+			.and().formLogin();
+	}
+	
+}
